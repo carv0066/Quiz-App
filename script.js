26b840f4914f25c1e010ff983e0f6f035ad6a805
Carlos Carvajal <carv0066@algonquinlive.com>
2025-04-04 05:14:56 -0400

    Worked on Creating new buttons

--- script.js
+++ script.js
@@ -2,47 +2,36 @@ const question = document.querySelector(".question");
 const incorrect = document.querySelectorAll(".incorrect");
 const correct = document.querySelector(".correct");
 const buttons = document.querySelectorAll(".buttons");
+const answersList = document.querySelector(".answer-list");
 
 //Fetch the data from an API
 const apiUrl = "https://opentdb.com/api.php?amount=10";
 // Make a GET request using the Fetch API
-// async function fetchQuestions() {
-//     try {
-//         const response = await fetch(apiUrl);
-//         const quizData = await response.json();
-//         return quizData;
-//     } catch (error) {
-//         console.error("An error ocurred", error);
-//     }
-// }
-// //Grab data Fetched and replace placeholder content(Question and 4 answers);
-// fetchQuestions().then(quizInfo => {
-//     //Quiz data is the same as the info
-//     console.log("Data is", quizInfo)
-//     console.log("Question is:", quizInfo.results[0].question);
-//     question.textContent = quizInfo.results[0].question;
+async function fetchQuestions() {
+    try {
+        const response = await fetch(apiUrl);
+        const quizData = await response.json();
+        return quizData;
+    } catch (error) {
+        console.error("An error ocurred", error);
+    }
+}
+//Grab data Fetched and replace placeholder content(Question and 4 answers);
+fetchQuestions().then(quizInfo => {
+    //Quiz data is the same as the info
+    console.log("Data is", quizInfo)
+    console.log("Question is:", quizInfo.results[0].question);
+    question.textContent = quizInfo.results[0].question;
 
-//     incorrect.forEach(wrong => {
-//         //Loop trough the wrong answers and Log each answer in a separate button
-//         wrong.textContent = quizInfo.results[0].incorrect_answers.splice(0,1);
-//     })
-//     correct.textContent = quizInfo.results[0].correct_answer;
+    incorrect.forEach(wrong => {
+        //Loop trough the wrong answers and Log each answer in a separate button
+        wrong.textContent = quizInfo.results[0].incorrect_answers;
 
-// //when Clicking on an answer it should tell me the correct and incorrect one
-//     buttons.forEach(button => {
-//         button.addEventListener('click', () => {
-//             if(button.classList.contains("incorrect")) {
-//                 console.log("the answers is wrong!!!!!!")
-//             } else {
-//                 console.log("The answer is correct!!!!")
-//             }
-//         })
-//     })
-// })
+    })
+    correct.textContent = quizInfo.results[0].correct_answer;
 
-//
-let buttonsArray = Array.from(buttons);
-console.log("Array", buttonsArray);
+    let buttonsArray = Array.from(buttons);
+    console.log("Array", buttonsArray);
 
 function randomAnswers() {
     let i = buttonsArray.length, j, temp;
@@ -55,16 +44,36 @@ function randomAnswers() {
         buttonsArray[j] = buttonsArray[i];
         buttonsArray[i] = temp;
     }
-        //Append items from the randomized array into the list, I know I can use map instead of forEach
-        console.log("Array Mofidied:",buttonsArray);
+        // Append items from the randomized array into the list, I know I can use map instead of forEach
+        console.log("Array Modified:",buttonsArray);
         buttonsArray.map(button => {
-            //I will need to create a new paragraph element for each answer
-            //I need to give each one of them their class
+            button.remove();
+
+
+            const newBtn = document.createElement("button");
+            newBtn.classList.add("buttons");
+            const btnText = document.createTextNode(button.textContent);
+            newBtn.appendChild(btnText);
+            answersList.appendChild(newBtn);
+
+            console.log("Is:",button)
+            console.log("are:", buttons[0])
         })
 }
-
 randomAnswers();
+// when Clicking on an answer it should tell me the correct and incorrect one
+//don't select it with the query selector but select it from the class
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if(btn.classList.contains("buttons")) {
+            console.log("the answers is wrong!!!!!!")
+        } else {
+            console.log("The answer is correct!!!!")
+        }
+    })
+})
 
+})
 //When clicking on right answer it should turn green
 //When clicking on wrong answer it should turn red, and the right answer green
 
